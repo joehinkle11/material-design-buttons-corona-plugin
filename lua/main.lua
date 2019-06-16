65b567944b5b64fd097717240457ed7e1941d969
Joseph Hinkle <joehinkle11@gmail.com>
2019-06-16 03:21:35 -0400

    initial commit

---
+++ lua/main.lua
@@ -0,0 +1,21 @@
+--
+-- Abstract: materialdesignbuttons Library Plugin Test Project
+--
+-- This is an example Corona Project documenting how
+-- to use the Material Design Buttons library.
+--
+-- Created by Joseph Hinkle
+--
+------------------------------------------------------------
+
+-- Load plugin library
+local materialdesignbuttons = require "plugin.materialdesignbuttons"
+
+-------------------------------------------------------------------------------
+-- BEGIN (Insert your sample test starting here)
+-------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------
+-- END
+-------------------------------------------------------------------------------
---
+++ lua/plugin/materialdesignbuttons.lua
@@ -0,0 +1,16 @@
+local Library = require "CoronaLibrary"
+
+-- Create library
+local lib = Library:new{ name='materialdesignbuttons', publisherId='io.joehinkle.materialdesignbuttons' }
+
+-------------------------------------------------------------------------------
+-- BEGIN (Insert your implementation starting here)
+-------------------------------------------------------------------------------
+
+
+-------------------------------------------------------------------------------
+-- END
+-------------------------------------------------------------------------------
+
+-- Return library instance
+return lib
